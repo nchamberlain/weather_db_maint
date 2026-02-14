71a7d6cfec85abcdf024584fe94a5bb757d2db50
Nelson <nelson.chamberlain7@gmail.com>
2026-02-14 15:54:32 -0800

    Now calculates Monthly, Fortnightly, and Weekly Averages

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use colorize::AnsiColor;
 use sqlx::{mysql::{MySqlPoolOptions, MySqlRow}, MySql, Pool, Row};
 use dotenvy::dotenv;
 use std::env;
-
+use std::io::{self, Write};
 
 fn main()  {
     let _ = get_db_action();
@@ -46,9 +46,9 @@ fn get_db_action() -> Result<(), sqlx::Error>{
         }*/ else if select == "Drop city sub tables" {
             println!("Dropping city sub tables...");
             drop_sub_tables().expect("Failed to drop city sub tables");
-        } else if select == "List all tables" {
-            println!("Listing all tables...");
-            // Add code to list all tables in the database
+        } else if select == "Insert Avgs into city sub tables" {
+            println!("Inserting averages into city sub tables...");
+            insert_averages().expect("Failed to insert averages into city sub tables");
         } else
         if select == "Exit" {
             println!("Exiting the program. Goodbye!");
@@ -239,4 +239,181 @@ async fn drop_city_sub_tables(pool: &Pool<MySql>, city: &str) -> Result<(), sqlx
     let _result = sqlx::query(&drop_stmt).execute(pool).await?;
 
     Ok(())
+}
+#[tokio::main] 
+async fn insert_averages() -> Result<(), sqlx::Error> {
+    // get the db env info from .env file
+    dotenv().ok();
+    // Set up the database URL from environment variable
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // Create a connection pool
+    let pool: Pool<MySql> = MySqlPoolOptions::new()
+        .max_connections(5) // Set the maximum number of connections
+        .connect(&database_url)
+        .await?;
+
+    let cities = vec![
+        "Berkeley_CA",
+        "Billings_MT",
+        "Bismarck_ND",
+        "Chicago_IL",
+        "Columbus_OH",
+        "Dallas_TX",
+        "Fairbanks_AK",
+        "Houston_TX",
+        "Indianapolis_IN",
+        "Jacksonville_FL",
+        "Los_Angeles_CA" ,
+        "Minneapolis_MN",
+        "Nashville_TN",
+        "New_York_NY",
+        "Oklahoma_OK",
+        "Philadelphia_PA",
+        "Phoenix_AZ",
+        "San_Antonio_TX",
+        "San_Diego_CA",
+        "San_Francisco_CA",
+        "Seattle_WA",
+        "Spokane_WA",
+    ];
+
+    let prompt_message = "Please select the cities to calculate averages for".green();
+    let selected_cities = inquire::MultiSelect::new(&prompt_message, cities)
+        .prompt()
+        .expect("Failed to select cities");
+
+    for the_city in selected_cities {
+        println!("Calculatng sub tables for city of {0}", the_city.red());
+
+        let mut first_year: i32 = 0;
+        let mut last_year: i32 = 0;
+
+        let first_year_result: Result<Vec<sqlx::mysql::MySqlRow>, sqlx::Error> = get_first_year(&pool, the_city).await;
+        match first_year_result {
+            Ok(_) => { 
+                let first_year_row = &first_year_result.unwrap(); //unwrap the row
+                let first_year_str: &str = first_year_row[0].get("tdate"); //get date string, for ex. 2020-09-05
+                first_year = first_year_str[0..4].parse().unwrap();  //parse first 4 digits as an int
+                println!("First year for {}: {}", the_city, first_year);
+            },
+            Err(e) => eprintln!("Error executing function: {}", e),
+        } 
+
+        let last_year_result: Result<Vec<sqlx::mysql::MySqlRow>, sqlx::Error> = get_last_year(&pool, the_city).await;
+        match last_year_result {
+            Ok(_) => { 
+                let last_year_row = &last_year_result.unwrap(); //unwrap the row
+                let last_year_str: &str = last_year_row[0].get("tdate"); //get date string, for ex. 2020-11-21
+                last_year = last_year_str[0..4].parse().unwrap();  //parse first 4 digits as an int
+                println!("Last year for {}: {}", the_city, last_year);
+            },
+            Err(e) => eprintln!("Error executing function: {}", e),
+        } 
+        calc_city_month(&pool, the_city, first_year, last_year).await?;    
+        calc_city_fort(&pool, the_city, first_year, last_year).await?;    
+        calc_city_week(&pool, the_city, first_year, last_year).await?;    
+    }
+
+    Ok(())
+}
+
+async fn calc_city_month(pool: &Pool<MySql>, city: &str, first_year: i32, last_year: i32) -> Result<(), sqlx::Error> {
+    let city_sub_month = format!("{city}_month");
+
+    for the_year in first_year..=last_year {
+        print!("{the_year},");
+        io::stdout().flush().unwrap(); // force flush now
+
+        for the_month in 1..=12 {
+            let insert_month = format!("INSERT INTO {city_sub_month} (station, tyear, tmonth, tmax, tmin) VALUES 
+((SELECT station from {city} WHERE tdate LIKE '{the_year}-{the_month:02}%' LIMIT 1),
+ {the_year}, 
+ {the_month}, 
+ round((select avg(tmax) from {city} WHERE tdate LIKE '{the_year}-{the_month:02}%')), 
+ round((select avg(tmin) from {city} WHERE tdate LIKE '{the_year}-{the_month:02}%')) );");
+            let _result = sqlx::query(&insert_month).execute(pool).await?;
+        }
+    }
+    Ok(())
+}
+async fn calc_city_fort(pool: &Pool<MySql>, city: &str, first_year: i32, last_year: i32) -> Result<(), sqlx::Error> {
+    let city_sub_fort = format!("{city}_fort"); 
+
+    for the_year in first_year..=last_year {
+        print!("{the_year}|");
+        io::stdout().flush().unwrap(); // force flush now
+
+        let mut low_fort = String::from("01-01");
+        for the_fort in 1..=26 {
+            let high_fort = get_next_fort(the_fort-1);
+            let low_date = format!("{}-{}", the_year, low_fort);
+            let high_date = format!("{}-{}", the_year, high_fort);
+
+
+            let insert_fort = format!("INSERT INTO {city_sub_fort} (station, tyear, tfort, tmax, tmin) VALUES 
+((SELECT station from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}' LIMIT 1),
+ {the_year}, 
+ {the_fort}, 
+ round((select avg(tmax) from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}')), 
+ round((select avg(tmin) from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}')) );");
+            let _result = sqlx::query(&insert_fort).execute(pool).await?;
+            low_fort = high_fort; //update low fort for next loop
+        }
+    }
+    Ok(())
+}
+async fn calc_city_week(pool: &Pool<MySql>, city: &str, first_year: i32, last_year: i32) -> Result<(), sqlx::Error> {
+    let city_sub_week = format!("{city}_week"); 
+
+    for the_year in first_year..=last_year {
+        print!("{the_year}-");
+        io::stdout().flush().unwrap(); // force flush now
+
+        let mut low_week = String::from("01-01");
+        for the_week in 1..=52 {
+            let high_week = get_next_week(the_week-1);
+            let low_date= format!("{}-{}", the_year, low_week);
+            let high_date= format!("{}-{}", the_year, high_week);
+        
+           let insert_week = format!("INSERT INTO {city_sub_week} (station, tyear, tweek, tmax, tmin) VALUES 
+((SELECT station from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}' LIMIT 1),
+ {the_year}, 
+ {the_week}, 
+ round((select avg(tmax) from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}')), 
+ round((select avg(tmin) from {city} WHERE tdate BETWEEN '{low_date}' AND '{high_date}')) );");
+            let _result = sqlx::query(&insert_week).execute(pool).await?;
+            low_week = high_week;            
+        }
+    }
+    Ok(())
+}
+
+async fn get_first_year(pool: &Pool<MySql>,  city: &str) -> Result<Vec<MySqlRow>, sqlx::Error> {
+    let query_stmt_string = format!("SELECT tdate FROM {city} order by tdate asc limit 1");
+    let rows: Vec<sqlx::mysql::MySqlRow> = sqlx::query(&query_stmt_string)
+        .fetch_all(pool)
+        .await?; 
+    //println!("Number of First Year Rows found: {}", rows.len());
+    Ok(rows)
+}
+
+async fn get_last_year(pool: &Pool<MySql>,  city: &str) -> Result<Vec<MySqlRow>, sqlx::Error> {
+    let query_stmt_string = format!("SELECT tdate FROM {city} order by tdate desc limit 1");
+    let rows: Vec<sqlx::mysql::MySqlRow> = sqlx::query(&query_stmt_string)
+        .fetch_all(pool)
+        .await?; // had to make this function return a Result to use the ? operator
+    //println!("Number of Last Year Rows found: {}", rows.len());
+    Ok(rows)
+}
+
+const FORTS:[&str; 26] = ["01-15", "01-29", "02-12", "02-26", "03-12", "03-26", "04-09", "04-23", "05-07", "05-21", "06-04", "06-18", "07-02", "07-16", "07-30", "08-13", "08-27", "09-10", "09-24", "10-08", "10-22", "11-05", "11-19", "12-03", "12-17", "12-32"];
+
+fn get_next_fort(idx: i32) -> String {
+    FORTS[(idx) as usize].to_string()   
+}
+
+const WEEKS:[&str; 52] = ["01-08", "01-15", "01-22", "01-29", "02-05", "02-12", "02-19", "02-26", "03-05", "03-12", "03-19", "03-26", "04-02", "04-09", "04-16", "04-23", "04-30", "05-07", "05-14", "05-21", "05-28", "06-04", "06-11", "06-18", "06-25", "07-02", "07-09", "07-16", "07-23", "07-30", "08-06", "08-13", "08-20", "08-27", "09-03", "09-10", "09-17", "09-24", "10-01", "10-08", "10-15", "10-22", "10-29", "11-05", "11-12", "11-19", "11-26", "12-03", "12-10", "12-17", "12-24", "12-32"];
+
+fn get_next_week(idx: i32) -> String {
+    WEEKS[(idx) as usize].to_string()   
 }
